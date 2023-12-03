a1eec533cb41af6c971e60b36ed15c7884483d55
Dean Rumsby <dean@deanrumsby.com>
2023-12-03 19:05:52 +0000

    refactor: clean up 2023/3

--- 2023/3/3.py
+++ 2023/3/3.py
@@ -3,118 +3,51 @@ import re
 with open("input.txt", "r") as file:
     lines = file.read().splitlines()
 
-DIRECTIONS = [
-    (1, 1),
-    (1, 0),
-    (1, -1),
-    (0, 1),
-    (0, -1),
-    (-1, 1),
-    (-1, 0),
-    (-1, -1),
-]
 
+def find_adjacent_matches(pattern, location):
+    i, (start_j, end_j) = location
+    adjacent_span = set(range(start_j - 1, end_j + 1))
 
-class EngineEntity:
-    def __init__(self, id, coords):
-        self.id = id
-        self.coords = coords
+    is_out_of_bounds = lambda i: i < 0 or i >= len(lines)
+    is_adjacent = lambda m: not adjacent_span.isdisjoint(set(range(m.start(), m.end())))
 
+    adjacent_matches = []
 
-class EngineNumber(EngineEntity):
-    def __init__(self, id, coords, value):
-        super().__init__(id, coords)
-        self.value = value
+    for di in [-1, 0, 1]:
+        i_bar = i + di
 
+        if is_out_of_bounds(i_bar):
+            continue
 
-class EngineSymbol(EngineEntity):
-    def __init__(self, id, coords, symbol):
-        super().__init__(id, coords)
-        self.symbol = symbol
+        matches = re.finditer(pattern, lines[i_bar])
+        adjacent_matches.extend(list(filter(is_adjacent, matches)))
 
-
-class EngineMap:
-    def __init__(self):
-        self.map = {}
-
-    def add(self, entity):
-        for i, j in entity.coords:
-            self.map[(i, j)] = entity
-
-    def adjacent(self, entity):
-        entities = []
-
-        for i, j in entity.coords:
-            for di, dj in DIRECTIONS:
-                i_bar = i + di
-                j_bar = j + dj
-
-                if (i_bar, j_bar) in entity.coords:
-                    continue
-                if not self.map.get((i_bar, j_bar)):
-                    continue
-
-                adjacent_entity = self.map[(i_bar, j_bar)]
-
-                if any(obj.id == adjacent_entity.id for obj in entities):
-                    continue
-
-                entities.append(adjacent_entity)
-
-        return entities
-
-    def is_adjacent_to_symbol(self, entity):
-        return any(isinstance(obj, EngineSymbol) for obj in self.adjacent(entity))
-
-    def adjacent_numbers(self, entity):
-        return list(
-            filter(lambda obj: isinstance(obj, EngineNumber), self.adjacent(entity))
-        )
-
-
-entity_count = 0
-engine_numbers = []
-engine_symbols = []
-engine_map = EngineMap()
-
-# parsing the schematics
-for i, line in enumerate(lines):
-    numbers = re.finditer(r"\d+", line)
-    symbols = re.finditer(r"[^\d\.]", line)
-
-    # creating EngineNumber objects
-    for number in numbers:
-        coords = [(i, j) for j in range(number.start(), number.end())]
-        engine_number = EngineNumber(entity_count, coords, int(number.group()))
-        engine_map.add(engine_number)
-        engine_numbers.append(engine_number)
-        entity_count += 1
-
-    # creating EngineSymbol objects
-    for symbol in symbols:
-        coords = [(i, symbol.start())]
-        engine_symbol = EngineSymbol(entity_count, coords, symbol.group())
-        engine_map.add(engine_symbol)
-        engine_symbols.append(engine_symbol)
-        entity_count += 1
+    return adjacent_matches
 
 
 part_numbers_sum = 0
-for number in engine_numbers:
-    if engine_map.is_adjacent_to_symbol(number):
-        part_numbers_sum += number.value
-
 gear_ratio_sum = 0
-for symbol in engine_symbols:
-    if symbol.symbol != "*":
-        continue
 
-    adjacent_numbers = engine_map.adjacent_numbers(symbol)
-    if len(adjacent_numbers) == 2:
-        gear_ratio = 1
-        for number in adjacent_numbers:
-            gear_ratio *= number.value
-        gear_ratio_sum += gear_ratio
+for i, line in enumerate(lines):
+    numbers = re.finditer(r"\d+", line)
+    potential_gears = re.finditer(r"\*", line)
+
+    for number in numbers:
+        adjacent_symbols = find_adjacent_matches(r"[^\.\d]", (i, number.span()))
+
+        if adjacent_symbols:
+            part_numbers_sum += int(number.group())
+
+    for pg in potential_gears:
+        adjacent_numbers = find_adjacent_matches(r"\d+", (i, pg.span()))
+
+        if len(adjacent_numbers) == 2:
+            gear_ratio = 1
+            for an in adjacent_numbers:
+                gear_ratio *= int(an.group())
+
+            gear_ratio_sum += gear_ratio
+
 
 print(f"Part 1: The sum of all the part numbers is {part_numbers_sum}")
 print(f"Part 2: The sum of all the gear ratios is {gear_ratio_sum}")
