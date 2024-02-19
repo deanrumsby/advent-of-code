7961082f1af69c107df0b0c24223ea9e88fbdb51
Dean Rumsby <dean@deanrumsby.com>
2024-02-19 12:18:19 +0000

    style: update 2015/4

--- 2015/4/4.py
+++ 2015/4/4.py
@@ -1,9 +1,47 @@
 # Advent of Code 2015
-# Day 4
+# Day 4: The Ideal Stocking Stuffer
+# Author: Dean Rumsby
 
 import hashlib
 
+################################################################################
+## Solutions
+################################################################################
+
 SECRET_KEY = "yzbqklnj"
+
+
+# Santa needs help mining some AdventCoins (very similar to bitcoins) to use as
+# gifts for all the economically forward-thinking little girls and boys.
+#
+# To do this, he needs to find MD5 hashes which, in hexadecimal, start with at
+# least five zeroes. The input to the MD5 hash is some secret key
+# (your puzzle input, given below) followed by a number in decimal. To mine
+# AdventCoins, you must find Santa the lowest positive number
+# (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+#
+# For example:
+#
+# - If your secret key is abcdef, the answer is 609043, because the MD5 hash of
+#   abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest
+#   such number to do so.
+# - If your secret key is pqrstuv, the lowest number it combines with to make
+#   an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of
+#   pqrstuv1048970 looks like 000006136ef....
+#
+def part1():
+    return mine(SECRET_KEY, 5)
+
+
+# Now find one that starts with six zeroes.
+#
+def part2():
+    return mine(SECRET_KEY, 6)
+
+
+################################################################################
+## Shared functions
+################################################################################
 
 
 def mine(key, difficulty):
@@ -19,16 +57,9 @@ def mine(key, difficulty):
         nonce += 1
 
 
-# To mine AdventCoins, you must find Santa the lowest positive number that
-# produces such a hash (a hash that starts with at least five leading zeroes)
-def part1():
-    return mine(SECRET_KEY, 5)
-
-
-# Now find one that starts with six zeroes.
-def part2():
-    return mine(SECRET_KEY, 6)
-
+################################################################################
+## Print answers
+################################################################################
 
 print(f"Part 1: The nonce required to mine the first AdventCoin is {part1()}")
 print(f"Part 2: The nonce required to mine the second AdventCoin is {part2()}")
