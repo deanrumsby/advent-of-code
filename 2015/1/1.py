4ec477e348c6105ab22365e3953b289ccc19068d
Dean Rumsby <dean@deanrumsby.com>
2024-02-19 14:07:30 +0000

    style: add question text and reorder all of 2015 solutions

--- 2015/1/1.py
+++ 2015/1/1.py
@@ -1,18 +1,33 @@
 # Advent of Code 2015
-# Day 1
+# Day 1: Not Quite Lisp
+# Author: Dean Rumsby
+
+################################################################################
+## Solutions
+################################################################################
 
 
-def change_floor(floor, direction):
-    match direction:
-        case "(":
-            return floor + 1
-        case ")":
-            return floor - 1
-        case _:
-            return floor
-
-
+# Santa is trying to deliver presents in a large apartment building, but he
+# can't find the right floor - the directions he got are a little confusing. He
+# starts on the ground floor (floor 0) and then follows the instructions one
+# character at a time.
+#
+# An opening parenthesis, (, means he should go up one floor, and a closing
+# parenthesis, ), means he should go down one floor.
+#
+# The apartment building is very tall, and the basement is very deep; he will
+# never find the top or bottom floors.
+#
+# For example:
+#
+# - (()) and ()() both result in floor 0.
+# - ((( and (()(()( both result in floor 3.
+# - ))((((( also results in floor 3.
+# - ()) and ))( both result in floor -1 (the first basement level).
+# - ))) and )())()) both result in floor -3.
+#
 # To what floor do the instructions take Santa?
+#
 def part1():
     with open("input.txt", "r") as file:
         directions = file.read()
@@ -25,8 +40,18 @@ def part1():
     return floor
 
 
-# What is the position of the character that causes Santa to first enter the
-# basement?
+# Now, given the same instructions, find the position of the first character
+# that causes him to enter the basement (floor -1). The first character in the
+# instructions has position 1, the second character has position 2, and so on.
+#
+# For example:
+#
+# - ) causes him to enter the basement at character position 1.
+# - ()()) causes him to enter the basement at character position 5.
+#
+# What is the position of the character that causes Santa to first enter
+# the basement?
+#
 def part2():
     with open("input.txt", "r") as file:
         directions = file.read()
@@ -42,6 +67,25 @@ def part2():
 
     return None
 
+
+################################################################################
+## Shared functions
+################################################################################
+
+
+def change_floor(floor, direction):
+    match direction:
+        case "(":
+            return floor + 1
+        case ")":
+            return floor - 1
+        case _:
+            return floor
+
+
+################################################################################
+## Print answers
+################################################################################
 
 print(f"Part 1: Santa is directed to floor {part1()}")
 print(f"Part 2: Santa first enters the basement at position {part2()}")
